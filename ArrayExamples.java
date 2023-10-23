f94170603b1515ba8e5eb37173ad55dc9916b54b
KongChengdesu <77406643+KongChengdesu@users.noreply.github.com>
2023-10-23 16:42:11 -0700

    Changed wrong reference

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -18,7 +18,7 @@ public class ArrayExamples {
     for(int i = 0; i < arr.length; i += 1) {
       newArray[arr.length - i - 1] = arr[i];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
